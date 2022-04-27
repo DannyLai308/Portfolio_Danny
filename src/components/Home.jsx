b909bc3628848a4dbbd80e343c09771cb1e3f350
Danny Lai <dannylai308@gmail.com>
2022-04-27 16:46:41 -0400

    home page created

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#87ceeb]'>
+
+      {/*Topline*/}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-[#494546]'>Hello, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Danny Lai
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I am a passionate Frontend Web Developer.
+        </h2>
+        <p className='text-[#494546] py-4 max-w-[700px]'>
+          I am specializing in designing and building exceptional UI experiences. What I am currently focusing
+          on are building responsive front-end web applications and documenting software requirements specification.
+        </p>
+        <div>
+            <button className='text-[#8892b0] font-bold group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#F7DC6F] hover:border-[#F7DC6F]'>
+              View My Works 
+              <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-2'/>
+              </span>
+              
+            </button>
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
 import Logo from '../assets/logo.PNG'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
     const[nav, setNav] = useState(false)
@@ -8,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#87ceeb] text-gray-800'>
         <div>
-            <img src={Logo} alt="Logo Image" style={{width: '100px'}}/>
+            <img src={Logo} alt="Logo Image" style={{width: '120px'}}/>
         </div>
 
         {/* Pages Menu*/}
@@ -36,8 +38,29 @@ const Navbar = () => {
         </ul>
 
         {/* Socail Icons*/}
-        <div className='hidden'>
-
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+            <ul>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#188EDD]'>
+                    <a className='flex justify-between items-center w-full text-gray-800' href="/">
+                        LinkedIn <FaLinkedin size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#14E062]'>
+                    <a className='flex justify-between items-center w-full text-gray-800' href="/">
+                        GitHub <FaGithub size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#FA5910]'>
+                    <a className='flex justify-between items-center w-full text-gray-800' href="/">
+                        Email <HiOutlineMail  size={30}/>
+                    </a>
+                </li>
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#F7F006]'>
+                    <a className='flex justify-between items-center w-full text-gray-800' href="/">
+                        Resume <BsFillPersonLinesFill  size={30}/>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
   )
